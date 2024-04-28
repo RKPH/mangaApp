a21650fcc4c31029a5a4f34959b9b8615ac329b9
RKPH <pnghung2003@gmail.com>
2024-04-28 12:34:48 +0700

    fix ui 13th

--- src/Layout/DefaultLayout/index.jsx
+++ src/Layout/DefaultLayout/index.jsx
@@ -1,27 +1,28 @@
 import PropTypes from "prop-types";
 import Header from "../../Components/Header";
-import { useState, useEffect , useRef} from "react";
+import { useState, useEffect, useRef } from "react";
 
 //icon
-import HomeIcon from '@mui/icons-material/Home';
-import CollectionsBookmarkIcon from '@mui/icons-material/CollectionsBookmark';
-import MenuBookIcon from '@mui/icons-material/MenuBook';
-import WhatshotIcon from '@mui/icons-material/Whatshot';
-import NewReleasesIcon from '@mui/icons-material/NewReleases';
-import SettingsIcon from '@mui/icons-material/Settings';
-import EuroIcon from '@mui/icons-material/Euro';
-import Face6Icon from '@mui/icons-material/Face6';
+import HomeIcon from "@mui/icons-material/Home";
+import CollectionsBookmarkIcon from "@mui/icons-material/CollectionsBookmark";
+import MenuBookIcon from "@mui/icons-material/MenuBook";
+import WhatshotIcon from "@mui/icons-material/Whatshot";
+import NewReleasesIcon from "@mui/icons-material/NewReleases";
+import SettingsIcon from "@mui/icons-material/Settings";
+import EuroIcon from "@mui/icons-material/Euro";
+import Face6Icon from "@mui/icons-material/Face6";
 
-import "./index.css";
-import { Link ,useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 const DefaultLayout = ({ children }) => {
   const location = useLocation();
 
   // Khởi tạo activeTab từ URL
-  const [activeTab, setActiveTab] = useState(location.pathname.split('/')[2] || 'Home');
+  const [activeTab, setActiveTab] = useState(
+    location.pathname.split("/")[2] || "Home"
+  );
 
   useEffect(() => {
-    const currentTab = location.pathname.split('/')[2] || 'Home';
+    const currentTab = location.pathname.split("/")[2] || "Home";
     if (currentTab !== activeTab) {
       setActiveTab(currentTab);
     }
@@ -30,14 +31,11 @@ const DefaultLayout = ({ children }) => {
   // Lưu trạng thái mới của activeTab vào localStorage
   const handleTabClick = (tabName) => {
     setActiveTab(tabName);
-    
-
   };
   return (
-    
     <div className="w-full h-screen ">
       <Header className="w-full   bg-white" />
-      <div className="w-full h-full  flex lg:flex-row flex-col">
+      <div className="w-full h-full  flex lg:flex-row ">
         <div className="hidden lg:block lg:w-1/5 mt-20  lg:left-0  lg:fixed  h-full bg-orange-600 ">
           {/* Menu for manga */}
           <div className=" h-[250px]  w-full mt-20 px-5">
@@ -45,39 +43,73 @@ const DefaultLayout = ({ children }) => {
               Menu -
             </div>
             <div className="w-full h-[280px]  ">
-
-              <Link to="/Home" className={`h-12 w-full flex items-center  px-2 text-gray-50 text-lg font-bold font-['Lato'] tracking-wide ${activeTab === 'Home' ? 'bg-yellow-600' : ''}  cursor-pointer  hover:bg-yellow-600 item my-1 transition duration-300`}        
-              onClick={() => {
-                localStorage.removeItem('currentPage')
-                handleTabClick('Home')
-              }}
+              <Link
+                to="/Home"
+                className={`h-12 w-full flex items-center  px-2 text-gray-50 text-lg font-bold font-['Lato'] tracking-wide ${
+                  activeTab === "Home" ? "bg-yellow-600" : ""
+                }  cursor-pointer  hover:bg-yellow-600 item my-1 transition duration-300`}
+                onClick={() => {
+                  localStorage.removeItem("currentPage");
+                  handleTabClick("Home");
+                }}
               >
-                <span className="mr-2 flex items-center"><HomeIcon/></span> Home
+                <span className="mr-2 flex items-center">
+                  <HomeIcon />
+                </span>{" "}
+                Home
               </Link>
 
-              <Link to='/danh-sach/the-loai' className={`h-12 w-full flex items-center  px-2 text-gray-50 text-lg font-bold font-['Lato'] tracking-wide ${activeTab === 'the-loai' ? 'bg-yellow-600' : ''}  cursor-pointer  hover:bg-yellow-600 item my-1 transition duration-300`}
-              onClick={() => {
-                localStorage.removeItem('currentPage')
-                handleTabClick('the-loai')}}
+              <Link
+                to="/danh-sach/the-loai"
+                className={`h-12 w-full flex items-center  px-2 text-gray-50 text-lg font-bold font-['Lato'] tracking-wide ${
+                  activeTab === "the-loai" ? "bg-yellow-600" : ""
+                }  cursor-pointer  hover:bg-yellow-600 item my-1 transition duration-300`}
+                onClick={() => {
+                  localStorage.removeItem("currentPage");
+                  handleTabClick("the-loai");
+                }}
               >
-                <span className="mr-2 flex items-center"><CollectionsBookmarkIcon/></span> Discover comics
+                <span className="mr-2 flex items-center">
+                  <CollectionsBookmarkIcon />
+                </span>{" "}
+                Discover comics
               </Link>
 
-              <Link to="/danh-sach/truyen-moi" className={`h-12 w-full flex items-center  px-2 text-gray-50 text-lg font-bold font-['Lato'] tracking-wide ${activeTab === 'truyen-moi' ? 'bg-yellow-600' : ''}  cursor-pointer  hover:bg-yellow-600 item my-1 transition duration-300`}     
-              onClick={() => handleTabClick('truyen-moi')}
+              <Link
+                to="/danh-sach/truyen-moi"
+                className={`h-12 w-full flex items-center  px-2 text-gray-50 text-lg font-bold font-['Lato'] tracking-wide ${
+                  activeTab === "truyen-moi" ? "bg-yellow-600" : ""
+                }  cursor-pointer  hover:bg-yellow-600 item my-1 transition duration-300`}
+                onClick={() => handleTabClick("truyen-moi")}
               >
-                <span className="mr-2 flex items-center"><MenuBookIcon/></span> Truyen moi
+                <span className="mr-2 flex items-center">
+                  <MenuBookIcon />
+                </span>{" "}
+                Truyen moi
               </Link>
 
-              <Link to="/danh-sach/dang-phat-hanh" className={`h-12 w-full flex items-center  px-2 text-gray-50 text-lg font-bold font-['Lato'] tracking-wide ${activeTab === 'dang-phat-hanh' ? 'bg-yellow-600' : ''}  cursor-pointer  hover:bg-yellow-600 item my-1 transition duration-300`}     
-              onClick={() => handleTabClick('dang-phat-hanh')}
+              <Link
+                to="/danh-sach/dang-phat-hanh"
+                className={`h-12 w-full flex items-center  px-2 text-gray-50 text-lg font-bold font-['Lato'] tracking-wide ${
+                  activeTab === "dang-phat-hanh" ? "bg-yellow-600" : ""
+                }  cursor-pointer  hover:bg-yellow-600 item my-1 transition duration-300`}
+                onClick={() => handleTabClick("dang-phat-hanh")}
               >
-                <span className="mr-2 flex items-center"><WhatshotIcon/></span> Đang phát hành
+                <span className="mr-2 flex items-center">
+                  <WhatshotIcon />
+                </span>{" "}
+                Đang phát hành
               </Link>
-              <div className={`h-12 w-full flex items-center  px-2 text-gray-50 text-lg font-bold font-['Lato'] tracking-wide ${activeTab === 'Sắp ra mắt' ? 'bg-yellow-600' : ''}  cursor-pointer  hover:bg-yellow-600 item my-1 transition duration-300`}     
-              onClick={() => handleTabClick('Sắp ra mắt')}
+              <div
+                className={`h-12 w-full flex items-center  px-2 text-gray-50 text-lg font-bold font-['Lato'] tracking-wide ${
+                  activeTab === "Sắp ra mắt" ? "bg-yellow-600" : ""
+                }  cursor-pointer  hover:bg-yellow-600 item my-1 transition duration-300`}
+                onClick={() => handleTabClick("Sắp ra mắt")}
               >
-                <span className="mr-2 flex items-center"><NewReleasesIcon/></span> Sắp ra mắt
+                <span className="mr-2 flex items-center">
+                  <NewReleasesIcon />
+                </span>{" "}
+                Sắp ra mắt
               </div>
             </div>
           </div>
@@ -87,26 +119,31 @@ const DefaultLayout = ({ children }) => {
               General -
             </div>
             <div className="w-full h-[148px]  ">
-
               <div className="h-12 w-full flex items-center  px-2 text-gray-50 text-lg font-bold font-['Lato'] tracking-wide  cursor-pointer  hover:bg-yellow-600 transition duration-300">
-                <span className="mr-2 flex items-center"><SettingsIcon/></span> Settings
+                <span className="mr-2 flex items-center">
+                  <SettingsIcon />
+                </span>{" "}
+                Settings
               </div>
 
               <div className="h-12 w-full flex items-center  px-2 text-gray-50 text-lg font-bold font-['Lato'] tracking-wide  cursor-pointer  hover:bg-yellow-600 transition duration-300">
-                <span className="mr-2 flex items-center"><EuroIcon/></span> Donate
+                <span className="mr-2 flex items-center">
+                  <EuroIcon />
+                </span>{" "}
+                Donate
               </div>
 
               <div className="h-12 w-full flex items-center  px-2 text-gray-50 text-lg font-bold font-['Lato'] tracking-wide  cursor-pointer  hover:bg-yellow-600 transition duration-300">
-                <span className="mr-2 flex items-center"><Face6Icon/></span> Sign in
+                <span className="mr-2 flex items-center">
+                  <Face6Icon />
+                </span>{" "}
+                Sign in
               </div>
-             
             </div>
           </div>
         </div>
-        <div  className="lg:w-4/5 h  w-full overflow-y-auto mt-20  z-0 lg:ml-[20%]">
-          
-             {children}
-         
+        <div className="lg:w-4/5 w-full overflow-y-auto mt-20  z-0 lg:ml-[20%]">
+          {children}
         </div>
       </div>
     </div>
