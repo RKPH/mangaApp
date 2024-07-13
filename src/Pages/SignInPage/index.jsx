6d084032e13224b6e1f00942ae3df3ab5a8ac853
RKPH <pnghung2003@gmail.com>
2024-07-13 20:18:58 +0700

    sua login google

--- src/Pages/SignInPage/index.jsx
+++ src/Pages/SignInPage/index.jsx
@@ -1,82 +1,115 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
-import { useDispatch, useSelector } from 'react-redux'
-import { useNavigate } from 'react-router'
-import { registerUser, registerUserWithGoogle } from '../../Redux/authSlice'
-import GoogleSignUp from './GoogleSignUp'
-import FormInput from './FormInput'
-import AuthLinks from './AuthLinks'
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router";
+import { registerUser, registerUserWithGoogle } from "../../Redux/authSlice";
+import GoogleSignUp from "./GoogleSignUp";
+import FormInput from "./FormInput";
+import AuthLinks from "./AuthLinks";
 
 function SignUpForm() {
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [confirmPassword, setConfirmPassword] = useState('')
-  const [emailError, setEmailError] = useState('')
-  const [error, setError] = useState('')
-  const auth = useSelector((state) => state.auth)
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [error, setError] = useState("");
+  const auth = useSelector((state) => state.auth);
 
   useEffect(() => {
     if (auth.isAuthenticated) {
-      navigate('/Home')
+      navigate("/Home");
     }
-  }, [auth.isAuthenticated, navigate])
+  }, [auth.isAuthenticated, navigate]);
 
   const responseGoogle = async (response) => {
-    const tokenId = response.credential
-    const result = await dispatch(registerUserWithGoogle(tokenId))
+    const tokenId = response.credential;
+    console.log(response);
+    const result = await dispatch(registerUserWithGoogle(tokenId));
     if (result.error) {
-      setError(result.error)
+      setError(result.error);
       setTimeout(() => {
-        setError('')
-      }, 5000)
+        setError("");
+      }, 5000);
     } else {
-      navigate('/Home')
+      navigate("/Home");
     }
-  }
+  };
 
   const handleSubmit = async (event) => {
-    event.preventDefault()
-    setEmailError('')
-    if (!email) setEmailError('Email cannot be empty')
+    event.preventDefault();
+    setEmailError("");
+    if (!email) setEmailError("Email cannot be empty");
     setTimeout(() => {
-      setEmailError('')
-    }, 5000)
+      setEmailError("");
+    }, 5000);
     if (email && password) {
-      const result = await dispatch(registerUser(name, email, password))
+      const result = await dispatch(registerUser(name, email, password));
       if (result.error) {
-        setError(result.error)
+        setError(result.error);
         setTimeout(() => {
-          setError('')
-        }, 5000)
+          setError("");
+        }, 5000);
       } else {
-        navigate('/Home')
+        navigate("/Home");
       }
     }
-  }
+  };
 
   return (
-    <div className='w-full flex flex-col justify-center bg-white dark:bg-[#18191A] py-4 z-0'>
-      <div className='h-screen w-full flex justify-center lg:px-10 px-4 py-2 bg-custom-image bg-cover bg-center bg-no-repeat'>
-        <form onSubmit={handleSubmit} className='form bg-white p-8 rounded-xl lg:w-[500px] w-full h-fit mt-10'>
-          {error && <span className='text-red-500 text-base'>{error}</span>}
-          <FormInput label='Username' value={name} onChange={setName} type='text' placeholder='Enter your username' />
-          {emailError && <span className='text-red-500 text-base'>{emailError}</span>}
-          <FormInput label='Email' value={email} onChange={setEmail} type='email' placeholder='Enter your email' />
-          <FormInput label='Password' value={password} onChange={setPassword} type='password' placeholder='Enter your password' />
-          <FormInput label='Confirm password' value={confirmPassword} onChange={setConfirmPassword} type='password' placeholder='Confirm your password' />
-          <button type='submit' className='button-submit bg-black text-white font-semibold py-2 w-full rounded-lg mt-6 hover:bg-gray-400 transition duration-200'>
+    <div className="w-full flex flex-col justify-center bg-white dark:bg-[#18191A] py-4 z-0">
+      <div className="h-screen w-full flex justify-center lg:px-10 px-4 py-2 bg-custom-image bg-cover bg-center bg-no-repeat">
+        <form
+          onSubmit={handleSubmit}
+          className="form bg-white p-8 rounded-xl lg:w-[500px] w-full h-fit mt-10"
+        >
+          {error && <span className="text-red-500 text-base">{error}</span>}
+          <FormInput
+            label="Username"
+            value={name}
+            onChange={setName}
+            type="text"
+            placeholder="Enter your username"
+          />
+          {emailError && (
+            <span className="text-red-500 text-base">{emailError}</span>
+          )}
+          <FormInput
+            label="Email"
+            value={email}
+            onChange={setEmail}
+            type="email"
+            placeholder="Enter your email"
+          />
+          <FormInput
+            label="Password"
+            value={password}
+            onChange={setPassword}
+            type="password"
+            placeholder="Enter your password"
+          />
+          <FormInput
+            label="Confirm password"
+            value={confirmPassword}
+            onChange={setConfirmPassword}
+            type="password"
+            placeholder="Confirm your password"
+          />
+          <button
+            type="submit"
+            className="button-submit bg-black text-white font-semibold py-2 w-full rounded-lg mt-6 hover:bg-gray-400 transition duration-200"
+          >
             Sign up
           </button>
-          <p className='text-center text-black text-sm my-3'>Or Sign Up With</p>
+          <p className="text-center text-black text-sm my-3">Or Sign Up With</p>
           <GoogleSignUp responseGoogle={responseGoogle} />
           <AuthLinks />
         </form>
       </div>
     </div>
-  )
+  );
 }
 
-export default SignUpForm
+export default SignUpForm;
