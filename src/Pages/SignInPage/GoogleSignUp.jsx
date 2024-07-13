537847148683f1e8989ba7a3f86955cc29754707
RKPH <pnghung2003@gmail.com>
2024-07-13 19:37:06 +0700

    sua login google

--- src/Pages/SignInPage/GoogleSignUp.jsx
+++ src/Pages/SignInPage/GoogleSignUp.jsx
@@ -1,23 +1,36 @@
 import React from "react";
-import { GoogleLogin } from "@react-oauth/google";
+import { useGoogleLogin } from "@react-oauth/google";
+import PropTypes from "prop-types";
 
 function GoogleSignUp({ responseGoogle }) {
+  const login = useGoogleLogin({
+    onSuccess: (codeResponse) => {
+      responseGoogle(codeResponse);
+    },
+    onError: () => {
+      console.log("Login Failed");
+    },
+    flow: "auth-code",
+    ux_mode: "redirect",
+    redirect_uri: "https://manga-app-steel.vercel.app/Signin", // Ensure this matches your Google API Console redirect URI
+  });
+
   return (
     <div className="w-full flex flex-row gap-4 my-6 items-center justify-center">
-      <GoogleLogin
-        onSuccess={(tokenResponse) => {
-          responseGoogle(tokenResponse);
-        }}
-        ux_mode="redirect"
-        redirect_uri="https://manga-app-steel.vercel.app/Signin"
-        type="icon"
-        size="large"
-        onError={() => {
-          console.log("Login Failed");
-        }}
-      />
+      <button onClick={() => login()} className="google-login-button">
+        <img
+          src="https://developers.google.com/identity/images/g-logo.png"
+          alt="Google Logo"
+          style={{ marginRight: "8px" }}
+        />
+        Sign in with Google
+      </button>
     </div>
   );
 }
+
+GoogleSignUp.propTypes = {
+  responseGoogle: PropTypes.func.isRequired,
+};
 
 export default GoogleSignUp;
