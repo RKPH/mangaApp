b8f13147231e0eda550429bab020fd2184dd354b
RKPH <pnghung2003@gmail.com>
2024-07-13 22:59:43 +0700

    sua sigin google

--- src/Pages/SignInPage/GoogleSignUp.jsx
+++ src/Pages/SignInPage/GoogleSignUp.jsx
@@ -2,18 +2,27 @@ import React from "react";
 import { GoogleLogin } from "@react-oauth/google";
 
 function GoogleSignUp({ responseGoogle }) {
+  const googleLogin = useGoogleLogin({
+    flow: "auth-code",
+    onSuccess: async (codeResponse) => {
+      console.log("Google login success:", codeResponse);
+      await responseGoogle(codeResponse);
+    },
+    onError: (errorResponse) => {
+      console.error("Google login error:", errorResponse);
+    },
+    ux_mode: "redirect", // Ensure this matches your setup in Google Developer Console
+    redirect_uri: "https://manga-app-steel.vercel.app/Signin", // Ensure this matches your Google API Console redirect URI
+  });
+
+  const handleGoogleLoginClick = () => {
+    console.log("Google login button clicked");
+    googleLogin(); // Trigger Google OAuth flow
+  };
+
   return (
-    <div className="w-full flex flex-row gap-4 my-6 items-center justify-center">
-      <GoogleLogin
-        onSuccess={(tokenResponse) => {
-          responseGoogle(tokenResponse);
-        }}
-        type="icon"
-        size="large"
-        onError={() => {
-          console.log("Login Failed");
-        }}
-      />
+    <div>
+      <button onClick={handleGoogleLoginClick}>Sign in with Google</button>
     </div>
   );
 }
