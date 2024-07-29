835010735082231678c1f8e6b59522ae02b34875
RKPH <pnghung2003@gmail.com>
2024-07-30 00:30:59 +0700

    fix User login

--- src/Redux/authSlice.js
+++ src/Redux/authSlice.js
@@ -1,169 +1,180 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit'
 
-const storedAuth = localStorage.getItem("auth");
+const storedAuth = localStorage.getItem('auth')
 const initialState = storedAuth
   ? JSON.parse(storedAuth)
   : {
       isAuthenticated: false,
-      token: null,
-    };
-
+      token: null
+    }
 export const authSlice = createSlice({
-  name: "auth",
+  name: 'auth',
   initialState,
   reducers: {
-    loginSuccess: (state) => {
-      state.isAuthenticated = true;
-      localStorage.setItem("auth", JSON.stringify(state));
+    loginSuccess: (state, action) => {
+      const { token } = action.payload
+      state.isAuthenticated = true
+      state.token = token
+      localStorage.setItem('auth', JSON.stringify(state))
     },
-    registerSuccess: (state) => {
-      state.isAuthenticated = true;
-      localStorage.setItem("auth", JSON.stringify(state));
+    registerSuccess: (state, action) => {
+      const { token } = action.payload
+      state.isAuthenticated = true
+      state.token = token
+      localStorage.setItem('auth', JSON.stringify(state))
     },
     logoutSuccess: (state) => {
-      state.isAuthenticated = false;
-      localStorage.removeItem("auth");
-    },
-  },
-});
-
+      state.isAuthenticated = false
+      state.token = null
+      localStorage.removeItem('auth')
+    }
+  }
+})
 export const { loginSuccess, logoutSuccess, registerSuccess } =
-  authSlice.actions;
-
+  authSlice.actions
 export const loginUser = (email, Password) => async (dispatch) => {
   try {
     const response = await fetch(
-      "https://itec-mangaapp-ef4733c4d23d.herokuapp.com/api/Auth/login",
+      'https://itec-mangaapp-ef4733c4d23d.herokuapp.com/api/Auth/login',
       {
-        method: "POST",
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ UserEmail: email, password: Password }),
-        credentials: "include", // Ensure cookies are included in the request
+        body: JSON.stringify({ UserEmail: email, password: Password })
       }
-    );
+    )
 
     if (!response.ok) {
-      const errorData = await response.text();
-      return { error: errorData || "Login failed" };
+      const errorData = await response.text()
+      return { error: errorData || 'Login failed' } // Return an error object instead of throwing an error
     }
 
-    const data = await response.json();
-    console.log(data);
+    const data = await response.json()
+    console.log(data)
     if (data.token) {
-      dispatch(loginSuccess());
-      return { success: true };
+      dispatch(
+        loginSuccess({
+          token: data.token
+        })
+      )
+      return { success: true }
     } else {
-      return { error: "Login failed: Invalid credentials" };
+      return { error: 'Login failed: Invalid credentials' }
     }
   } catch (error) {
-    console.log("Login failed:", error);
-    return { error: error.message || "Login failed" };
+    console.log('Login failed:', error)
+    return { error: error.message || 'Login failed' }
   }
-};
-
+}
 export const loginUserWithGoogle = (tokenId) => async (dispatch) => {
   try {
     const response = await fetch(
       `https://itec-mangaapp-ef4733c4d23d.herokuapp.com/api/Auth/Login/google?idToken=${tokenId}`,
       {
-        method: "POST",
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ idToken: tokenId }),
-        credentials: "include", // Ensure cookies are included in the request
+        body: JSON.stringify({ idToken: tokenId })
       }
-    );
+    )
 
     if (!response.ok) {
-      const errorData = await response.text();
-      return { error: errorData || "Login failed" };
+      const errorData = await response.text()
+      return { error: errorData || 'Register failed' } // Return an error object instead of throwing an error
     }
 
-    const data = await response.json();
-    console.log(data);
+    const data = await response.json()
+    console.log(data)
     if (data.token) {
-      dispatch(registerSuccess());
-      return { success: true };
+      dispatch(
+        registerSuccess({
+          token: data.token
+        })
+      )
+      return { success: true }
     } else {
-      return { error: "Login failed: Invalid credentials" };
+      return { error: 'Register failed: Invalid credentials' }
     }
   } catch (error) {
-    console.log("Login failed:", error);
-    return { error: error.message || "Login failed" };
+    console.log('Register failed:', error)
+    return { error: error.message || 'Register failed' }
   }
-};
-
+}
 export const registerUser = (username, email, password) => async (dispatch) => {
   try {
     const response = await fetch(
-      "https://itec-mangaapp-ef4733c4d23d.herokuapp.com/api/Auth/register",
+      'https://itec-mangaapp-ef4733c4d23d.herokuapp.com/api/Auth/register',
       {
-        method: "POST",
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json'
         },
         body: JSON.stringify({
           UserName: username,
           UserEmail: email,
-          password: password,
-        }),
-        credentials: "include", // Ensure cookies are included in the request
+          password: password
+        })
       }
-    );
+    )
 
     if (!response.ok) {
-      const errorData = await response.text();
-      return { error: errorData || "Register failed" };
+      const errorData = await response.text()
+      return { error: errorData || 'Register failed' } // Return an error object instead of throwing an error
     }
 
-    const data = await response.json();
-    console.log(data);
+    const data = await response.json()
+    console.log(data)
     if (data.token) {
-      dispatch(registerSuccess());
-      return { success: true };
+      dispatch(
+        registerSuccess({
+          token: data.token
+        })
+      )
+      return { success: true }
     } else {
-      return { error: "Register failed: Invalid credentials" };
+      return { error: 'Register failed: Invalid credentials' }
     }
   } catch (error) {
-    console.log("Register failed:", error);
-    return { error: error.message || "Register failed" };
+    console.log('Register failed:', error)
+    return { error: error.message || 'Register failed' }
   }
-};
-
+}
 export const registerUserWithGoogle = (tokenId) => async (dispatch) => {
   try {
     const response = await fetch(
       `https://itec-mangaapp-ef4733c4d23d.herokuapp.com/api/Auth/Register/google?idToken=${tokenId}`,
       {
-        method: "POST",
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ idToken: tokenId }),
-        credentials: "include", // Ensure cookies are included in the request
+        body: JSON.stringify({ idToken: tokenId })
       }
-    );
+    )
 
     if (!response.ok) {
-      const errorData = await response.text();
-      return { error: errorData || "Register failed" };
+      const errorData = await response.text()
+      return { error: errorData || 'Register failed' } // Return an error object instead of throwing an error
     }
 
-    const data = await response.json();
-    console.log(data);
+    const data = await response.json()
+    console.log(data)
     if (data.token) {
-      dispatch(registerSuccess());
-      return { success: true };
+      dispatch(
+        registerSuccess({
+          token: data.token
+        })
+      )
+      return { success: true }
     } else {
-      return { error: "Register failed: Invalid credentials" };
+      return { error: 'Register failed: Invalid credentials' }
     }
   } catch (error) {
-    console.log("Register failed:", error);
-    return { error: error.message || "Register failed" };
+    console.log('Register failed:', error)
+    return { error: error.message || 'Register failed' }
   }
-};
+}
 
-export default authSlice.reducer;
+export default authSlice.reducer
--- src/Redux/userSlice.js
+++ src/Redux/userSlice.js
@@ -1,23 +1,25 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from 'axios';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios'
 
 // Async thunk to fetch user data
 export const fetchUser = createAsyncThunk(
   'user/fetchUser',
-  async (_, { rejectWithValue }) => {
+  async (token, { rejectWithValue }) => {
     try {
       const response = await axios.get(
         'https://itec-mangaapp-ef4733c4d23d.herokuapp.com/api/Auth/profile',
         {
-          withCredentials: true,
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
         }
-      );
-      return response.data;
+      )
+      return response.data
     } catch (error) {
-      return rejectWithValue(error.response?.data);
+      return rejectWithValue(error.response?.data)
     }
   }
-);
+)
 
 // Async thunk to update user data
 export const updateUser = createAsyncThunk(
@@ -29,51 +31,50 @@ export const updateUser = createAsyncThunk(
         userData,
         {
           headers: {
-            'Content-Type': 'application/json',
-          },
-          withCredentials: true,
+            'Content-Type': 'application/json'
+          }
         }
-      );
-      return response.data;
+      )
+      return response.data
     } catch (error) {
-      return rejectWithValue(error.response.data);
+      return rejectWithValue(error.response.data)
     }
   }
-);
+)
 
 const userSlice = createSlice({
-  name: "user",
+  name: 'user',
   initialState: {
     user: null,
-    status: "idle",
-    error: null,
+    status: 'idle',
+    error: null
   },
   reducers: {},
   extraReducers: (builder) => {
     builder
       .addCase(fetchUser.pending, (state) => {
-        state.status = "loading";
+        state.status = 'loading'
       })
       .addCase(fetchUser.fulfilled, (state, action) => {
-        state.status = "succeeded";
-        state.user = action.payload;
+        state.status = 'succeeded'
+        state.user = action.payload
       })
       .addCase(fetchUser.rejected, (state, action) => {
-        state.status = "failed";
-        state.error = action.payload;
+        state.status = 'failed'
+        state.error = action.payload
       })
       .addCase(updateUser.pending, (state) => {
-        state.status = "loading";
+        state.status = 'loading'
       })
       .addCase(updateUser.fulfilled, (state, action) => {
-        state.status = "succeeded";
-        state.user = action.payload;
+        state.status = 'succeeded'
+        state.user = action.payload
       })
       .addCase(updateUser.rejected, (state, action) => {
-        state.status = "failed";
-        state.error = action.payload;
-      });
-  },
-});
+        state.status = 'failed'
+        state.error = action.payload
+      })
+  }
+})
 
-export default userSlice.reducer;
+export default userSlice.reducer
